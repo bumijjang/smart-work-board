15a2ac575dc24b6aeda2a76b8569c657c580d998
bumijjang <bumijjang@gmail.com>
2026-09-02 14:16:42 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,48 @@
-const CACHE_NAME = "smart-board-v1";
+const CACHE_NAME = "smart-board-v2";
 
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const FILES_TO_CACHE = [
+  "/smart-work-board/",
+  "/smart-work-board/index.html",
+  "/smart-work-board/manifest.json",
+  "/smart-work-board/icon-192.png",
+  "/smart-work-board/icon-512.png"
 ];
 
 
-self.addEventListener(
-"install",
-event => {
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
 
-event.waitUntil(
- caches.open(CACHE_NAME)
- .then(cache=>{
-   return cache.addAll(FILES);
- })
-);
-
+  self.skipWaiting();
 });
 
 
-self.addEventListener(
-"fetch",
-event=>{
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 
-event.respondWith(
- caches.match(event.request)
- .then(response=>{
-   return response || fetch(event.request);
- })
-);
+  self.clients.claim();
+});
 
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
